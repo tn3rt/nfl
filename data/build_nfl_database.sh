474b480d805a86270a7ed32f5f5bb9169fef9c9d
Trent <trent.brunson@gmail.com>
2014-08-31 12:02:48 -0400

    Added more CSV files with player and weather data. Also added complete 2013 season data and 2014 pre-season data. Changed the Bash script to insert player and weather data into separate collections.

--- data/build_nfl_database.sh
+++ data/build_nfl_database.sh
@@ -1,6 +1,18 @@
 #!bin/bash
-    FILES="*.csv"
-    for f in $FILES
+    PLAYS="*pbp*.csv"
+    PLAYERS="players*.csv"
+    WEATHER="weather*.csv"
+    for i in $PLAYS
     do
-        mongoimport -d nfl -c play --type csv --file "$f" --headerline
+        mongoimport -d nfl -c play --type csv --file "$i" --headerline
+    done
+    
+    for j in $PLAYERS
+    do
+        mongoimport -d nfl -c players --type csv --file "$j" --headerline
+    done
+
+    for k in $WEATHER
+    do
+        mongoimport -d nfl -c players --type csv --file "$k" --headerline
     done
