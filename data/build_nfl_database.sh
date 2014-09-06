0b67ff7c3f6c25cebe00e890d10546a0dc00b4f0
Trent <trent.brunson@gmail.com>
2014-09-06 12:34:52 -0400

    added combine data

--- data/build_nfl_database.sh
+++ data/build_nfl_database.sh
@@ -2,6 +2,7 @@
     PLAYS="*pbp*.csv"
     PLAYERS="players*.csv"
     WEATHER="weather*.csv"
+    COMBINE="combine*.csv"
     for i in $PLAYS
     do
         mongoimport -d nfl -c play --type csv --file "$i" --headerline
@@ -15,4 +16,9 @@
     for k in $WEATHER
     do
         mongoimport -d nfl -c weather --type csv --file "$k" --headerline
+    done
+
+    for l in $COMBINE
+    do
+        mongoimport -d nfl -c combine --type csv --file "$l" --headerline
     done
